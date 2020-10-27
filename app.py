5a678e6abce84b5535df268da8b2841e52097313
coachshaquan <60889862+coachshaquan@users.noreply.github.com>
2020-10-27 12:23:49 -0400

    Update app.py
    Minor spacing and comment arrangement changes.

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import requests
 import os
 import json
 
+# This app returns manual page data for terminal commands.
 app = Flask(__name__)
 filename = os.path.join(app.static_folder,'linux.json')
 with open(filename) as json_file:
     json_file = json.load(json_file)
-    
-#page for linux command searching 
+   
 @app.route('/',  methods=["GET","POST"])
 def index():       
     list_of_commands = list(map(lambda x:x['command'], json_file))
@@ -21,10 +21,9 @@ def search():
         command = request.form["search_bar"]
     elif request.method == "GET":
         command = request.args["cmd"]
-        
-    
+     
     search_data = list(filter(lambda x : x['command'] == command,
-                                  json_file))
+                              json_file))
     if len(search_data):
         search_data = search_data[0]
     else:
